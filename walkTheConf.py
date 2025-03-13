71f64e10a6c89dfe0948bc53410f6f8c50190aa9
Skottyskotch <alexandre.henry+github@m4am.net>
2025-03-13 01:30:15 +0100

    extract csv take1

--- walkTheConf.py
+++ walkTheConf.py
@@ -1,10 +1,13 @@
 import argparse
 import os
 import re
-import inquirer # pip install inquirer
+import inquirer # non-standard module required 'pip install inquirer'
+import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument("lspdir", help="lsp directory name (path/to/diffDPM/UNDEFINED)")
+parser.add_argument("-c", "--choose", action='store_true', help="prompt for selection of one object to search callers")
+parser.add_argument("-i", "--info", action='store_true', help="display information on regexp matching Names in lsps")
 args = parser.parse_args()
 
 class objectType:
@@ -18,11 +21,13 @@ class objectType:
 
 class objectLsp:
     instances = {}
-    def __init__(self, nOnb, oObjectType, path):
+    def __init__(self, nOnb, oObjectType, path, text):
         self.onb = nOnb
         self.name = b''
         self.path = path
         self.objectType = oObjectType
+        self.text = text
+        self.callers = []
         objectLsp.instances[self.onb] = self
         oObjectType.objects.append(self)
 
@@ -39,25 +44,30 @@ def findNames(lspdir):
                     with open(os.path.join(root, name, file), 'rb') as lsp:
                         lspText = lsp.read()
                         objectOnb = re.findall(regexB,lspText)[0]
-                        myObj = objectLsp(int(objectOnb.decode('utf-8')), myObjectType, os.path.join(root, name, file))
+                        myObj = objectLsp(int(objectOnb.decode('utf-8')), myObjectType, os.path.join(root, name, file), lspText)
                         if len(re.findall(regexA,lspText)) > 0:
                             objectType.instances[name].named = True
-                        if len(re.findall(refinedRegex1,lspText)) > 0:
-                            objectType.instances[name].regex = '<:NAME "OBJECT_NAME">'
-                            objectName = re.findall(refinedRegex1,lspText)[0]
-                            myObj.name = objectName
-                        elif len(re.findall(refinedRegex2,lspText)) > 0:
-                            objectType.instances[name].regex = '<:NAME :OBJECT_NAME>'
-                            objectName = re.findall(refinedRegex2,lspText)[0]
+                            objectName = b''
+                            if len(re.findall(refinedRegex1,lspText)) > 0:
+                                objectType.instances[name].regex = '<:NAME "OBJECT_NAME">'
+                                objectName = re.findall(refinedRegex1,lspText)[0]
+                            elif len(re.findall(refinedRegex2,lspText)) > 0:
+                                objectType.instances[name].regex = '<:NAME :OBJECT_NAME>'
+                                objectName = re.findall(refinedRegex2,lspText)[0]
                             myObj.name = objectName
     else:
         print(lspdir)
         print('not a dir')
         return False
-    print('Ignored:')
-    print('\n'.join(['\t'  + key for key, value in objectType.instances.items() if value.named == False]))
-    print('Parsed:')
-    print('\n'.join(['\t' + str(len(value.objects)) + '\t' + str(value.regex) + '\t' + key for key, value in objectType.instances.items() if value.named == True]))
+
+    if args.info:
+        print(':NAME not found:')
+        print('\n'.join(['\t' + str(len(value.objects)) + '\t' + key                            for key, value in objectType.instances.items() if value.named == False]))
+        print(':NAME parsed:')
+        print('\n'.join(['\t' + str(len(value.objects)) + '\t' + str(value.regex) + '\t' + key  for key, value in objectType.instances.items() if value.named == True]))
+        print('number of objects: ' + str(len(list(objectLsp.instances.keys()))))
+        input('Type Enter to continue...')
+        os.system('cls')
     return True
 
 def chooseObjectToSearch():
@@ -68,6 +78,7 @@ def chooseObjectToSearch():
             'class',
             message="Select one class",
             choices=[value.name + ' (' + str(len(value.objects)) + ')' for key, value in objectType.instances.items()],
+            carousel=True
         )]
         answers = inquirer.prompt(questions)  # returns a dict
         if len(answers['class']) == 1:
@@ -83,6 +94,7 @@ def chooseObjectToSearch():
             'object',
             message="Select one object",
             choices=[myObj.name.decode('utf-8') + ' (' + str(myObj.onb) + ')'for myObj in objectType.instances[sChosenClass].objects],
+            carousel=True
         )]
         answers = inquirer.prompt(questions)  # returns a dict
         if len(answers['object']) == 1:
@@ -93,28 +105,46 @@ def chooseObjectToSearch():
         else:
             print('Please select only one object')
 
-def searchDependancies(obj, lspdir):
-    print(obj.objectType.name + ' object ' + obj.name.decode('utf-8') + ' ('+ str(obj.onb) +') searched in the conf')
+def searchDependancies(obj, verbose):
     callers = []
-    for root, dirs, files in os.walk(lspdir, topdown=False):
-        for name in dirs:
-            for fileName in files:
-                for file in os.listdir(os.path.join(root,name)):
-                    with open(os.path.join(root, name, file), 'rb') as lsp:
-                        if obj.path != os.path.join(root, name, file):
-                            text = lsp.read()
-                            if len(re.findall(str(obj.onb).encode('utf-8'), text)) > 0:
-                                print('onb found in ' + os.path.join(root, name, file))
-                            if obj.objectType.named == True and len(re.findall(obj.name, text)) > 0:
-                                print('name found in ' + os.path.join(root, name, file))
+    for otherObj in list(objectLsp.instances.values()):
+        if obj.onb != otherObj.onb:
+            if len(re.findall(str(obj.onb).encode('utf-8'), otherObj.text)) > 0:
+                if verbose:
+                    print('onb found in ' + otherObj.path)
+                callers.append(otherObj)
+            if obj.objectType.named == True and len(re.findall(obj.name, otherObj.text)) > 0:
+                if verbose:
+                    print('name found in ' + otherObj.path)
+                callers.append(otherObj)
+    obj.callers = callers
+
+def dumpCallers():
+    onbs = list(objectLsp.keys())
+    data = {'ONB': onbs,
+            'NAME': [objectLsp[x].name for x in onbs],
+            'CALLERS': [','.join(objectLsp[x].callers) for x in onbs]}
+    df = pd.DataFrame(data)
+    df.to_csv('callers.csv')
 
 def __main__():
     print('Find the names of the objects')
     lspdir = args.lspdir
     if findNames(lspdir):
-        print('Search dependencies for one object')
-        myObj = chooseObjectToSearch()
-        searchDependancies(myObj, lspdir)
+        if args.choose:
+            print('Search callers for one object')
+            myObj = chooseObjectToSearch()
+            print(myObj.objectType.name + ' object "' + myObj.name.decode('utf-8') + '" ('+ str(myObj.onb) +') searched in the conf')
+            searchDependancies(myObj, True)
+            print('\n'+str(len(myObj.callers)) + ' callers:')
+            print('\n'.join([x.objectType.name + '\t' + str(x.onb) for x in myObj.callers]))
+        else:
+            i = 0
+            for obj in list(objectLsp.instances.values()):
+                i += 1
+                print('searching dependencies ' + str(i) + '/' + str(len(list(objectLsp.instances.values()))), end = '\r')
+                searchDependancies(obj, False)
+            dumpCallers()
 
 nameRegex =         b'^\s*:NAME\s+(.*)$'
 refinedNameRegex1 = b'^\s*:NAME\s+"([^"]*)"$'
