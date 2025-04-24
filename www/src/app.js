d17eaede0b59411b177af8ad54464cc52218d46f
Skottyskotch <alexandre.henry+github@m4am.net>
2025-04-25 00:48:14 +0200

    introduce some layout tests (numpad keys: 1, 2)

--- www/src/app.js
+++ www/src/app.js
@@ -8,7 +8,6 @@ async function fetchNode(id){
 	const response = await fetch('http://localhost:3000/api/graph-data/node?ID='+id);
 	const data = await response.json();
 	var attributes = data.node[0].ATTRIBUTES.split('|');
-	console.log(data.node);
 	var values = data.node[0].DATA.split('|');
 	var title = data.node[0].TYPE;
 
@@ -65,6 +64,7 @@ document.addEventListener('DOMContentLoaded', function() {
 				group: 'edges', data: {id: edge.source + '_' + edge.target, source: edge.source, target: edge.target, inattr: edge.inattr, label: edge.inattr.substring(1) + foundby}
 			}
 		});
+		// cytoscape.use(window.cytoscapeFcose);
 		var cy = cytoscape({
 			container: document.getElementById('cy'), // container to render in
 
@@ -100,7 +100,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 			,layout: {
 				name: 'breadthfirst',
-
 				fit: true, // whether to fit the viewport to the graph
 				directed: false, // whether the tree is directed downwards (or edges can point in any direction if false)
 				padding: 30, // padding on fit
@@ -157,6 +156,46 @@ document.addEventListener('DOMContentLoaded', function() {
 			document.querySelectorAll(`[class*="kv-"]`).forEach(el => {
 				el.classList.remove('highlight-yellow');
 			});
+		});
+
+		document.addEventListener('keydown', (event) => {
+			const key = event.key;
+
+			if (key === '1') {
+				console.log('Layout : fcose');
+				cy.layout({
+					name: 'fcose',
+					animate: false,
+					nodeRepulsion: 100000,
+					idealEdgeLength: 250,
+					gravity: 0.01
+				}).run();
+			}
+
+			if (key === '2') {
+				console.log('Layout : breadthfirst');
+				cy.layout({
+					name: 'breadthfirst',
+					fit: true, // whether to fit the viewport to the graph
+					directed: false, // whether the tree is directed downwards (or edges can point in any direction if false)
+					padding: 30, // padding on fit
+					circle: false, // put depths in concentric circles if true, put depths top down if false
+					grid: false, // whether to create an even grid into which the DAG is placed (circle:false only)
+					spacingFactor: 1.75, // positive spacing factor, larger => more space between nodes (N.B. n/a if causes overlap)
+					boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+					avoidOverlap: true, // prevents node overlap, may overflow boundingBox if not enough space
+					nodeDimensionsIncludeLabels: false, // Excludes the label when calculating node bounding boxes for the layout algorithm
+					roots: undefined, // the roots of the trees
+					depthSort: undefined, // a sorting function to order nodes at equal depth. e.g. function(a, b){ return a.data('weight') - b.data('weight') }
+					animate: false, // whether to transition the node positions
+					animationDuration: 500, // duration of animation in ms if enabled
+					animationEasing: undefined, // easing of animation if enabled,
+					animateFilter: function ( node, i ){ return true; }, // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
+					ready: undefined, // callback on layoutready
+					stop: undefined, // callback on layoutstop
+					transform: function (node, position ){ return position; } // transform a given node position. Useful for changing flow direction in discrete layouts
+				}).run();
+			}
 		});
 	});
 });
